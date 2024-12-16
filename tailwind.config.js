b35d4d3ed85d567ff7215b897b47630afb6559c3
AHMAD-AMINU <ahmadaminu757@gmail.com>
2024-12-16 14:22:09 +0100

    desktop view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
     
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'primary-color':'F5F7F8',
+        'secondry-color':'1230AE',
+
+      },
+      fontFamily: {
+        "Poppins": ["Poppins", "sans-serif"]
+      }
     },
+    container: { 
+      center: true,
+      padding: '1rem',
+       screens: { sm: '100%', 
+         md: '100%', 
+          lg: '1024px', 
+          xl: '1280px', },
+       },
+
   },
   plugins: [],
 }
